962e4996aac16436865cb3f482dad727225e2b63
yash_palkar <111225341+yashpalkar2001@users.noreply.github.com>
2025-08-04 12:08:10 +0530

    Update formAndFaq.tsx
    added API need to check

--- src/app/landingPage/formAndFaq.tsx
+++ src/app/landingPage/formAndFaq.tsx
@@ -90,7 +90,7 @@ export default function FormAndFaq() {
     };
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/submit-contact/", {
+      const res = await fetch("https://biochar-api.onrender.com/api/contacts", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
